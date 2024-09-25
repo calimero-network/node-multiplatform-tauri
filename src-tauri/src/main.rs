c4d140f698c8584a337a7909a41b2652d2bfc784
alenmestrov <alenmestrov@gmail.com>
2024-09-25 10:13:15 +0200

    feat: main.rs file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,32 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::Manager;
+use utils::setup::{setup_app_state, setup_store};
 
-fn main() {
+mod commands;
+mod error;
+mod operations;
+mod store;
+mod types;
+mod utils;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .setup(|app| {
+            let app_handle = app.handle();
+            // Initialize store
+            let store = setup_store(app)?;
+            // Initialize app state
+            let app_state = setup_app_state(app_handle, store)?;
+            app.manage(app_state);
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![
+            commands::node_commands::initialize_node,
+            commands::node_commands::fetch_nodes,
+        ])
+        .run(tauri::generate_context!())?;
+
+    Ok(())
 }
